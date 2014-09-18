0c5258bba07aae335fdcac065fb2dcc6c0ea9a23
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2014-09-18 20:48:25 +0000

    Added in more fields, made HTML form easier to edit

--- peacecorps/peacecorps/forms.py
+++ peacecorps/peacecorps/forms.py
@@ -14,6 +14,28 @@ class DonationPaymentForm(forms.Form):
         ('credit-card', 'Credit Card'), 
         ('ach-bank-check', 'ACH Bank Check'),
     )
+
+    DEDICATION_TYPE_CHOICES = (
+        ('in-honor', 'In Honor'),
+        ('in-memory', 'In Memory')
+    )
+
+    ded_yes = "I authorize the Peace Corps to make my name and contact"
+    ded_yes += " information available to the honoree."
+
+    ded_no = "I do not authorize the Peace Corps to make my name and contact"
+    ded_no += " information available to the honoree."
+    
+    DEDICATION_CONSENT_CHOICES = (
+        ('yes-dedication-consent', ded_yes),
+        ('no-dedication-consent', ded_no),
+    )
+
+
+    VOLUNTEER_CONSENT_CHOICES = (
+        ('vol-consent-yes', 'Share with Volunteer'), 
+        ('vol-consent-no', "Don't share with Volunteer")
+    )
     
     donor_type = forms.ChoiceField(
         widget=forms.RadioSelect, choices=DONOR_TYPE_CHOICES,
@@ -26,7 +48,34 @@ class DonationPaymentForm(forms.Form):
         widget=forms.RadioSelect, choices=PAYMENT_TYPE_CHOICES, 
         initial="credit-card",
     )
-    comments = forms.CharField(max_length=150, required=False)
+    comments = forms.CharField(
+        max_length=150,
+        required=False,
+        widget=forms.Textarea(attrs={'rows':4}))
+
+    # User consents to stay informed about the Peace Corps. 
+    email_consent = forms.BooleanField(initial=True)
+
+    # True if there might be a possible conflict of interest. 
+    interest_conflict = forms.BooleanField(initial=False)
+
+    #Dedication related fields
+    dedication = forms.BooleanField(initial=False)
+    dedication_name = forms.CharField(label="Name", max_length=40)
+
+    dedication_type = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=DEDICATION_TYPE_CHOICES,
+        initial='in-honor')
+    dedication_email = forms.EmailField(label="Email", required=False)
+    dedication_address = forms.CharField(
+        label="Mailing Address", max_length=255, required=False)
+    card_dedication = forms.CharField(max_length=150, required=False)
+    dedication_consent = forms.ChoiceField(
+        widget=forms.RadioSelect, initial='yes-dedication-consent',
+        choices=DEDICATION_CONSENT_CHOICES)
+    information_consent = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=VOLUNTEER_CONSENT_CHOICES,
+        initial='vol-consent-yes')
 
 class USDonationPaymentForm(DonationPaymentForm):
     """ A US address specific donation payment form. Since most of the donors
