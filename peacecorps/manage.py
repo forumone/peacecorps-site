430ce6eeef7f7484d4bdd735142ba7cda1ee7686
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2014-09-25 17:01:16 +0000

    Make the dev settings default

--- peacecorps/manage.py
+++ peacecorps/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peacecorps.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peacecorps.settings.dev")
 
     from django.core.management import execute_from_command_line
 
