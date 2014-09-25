e3afe64ae33feadfac601fc32df67ce7e0385421
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2014-09-25 19:29:29 +0000

    Fix test to add country

--- peacecorps/peacecorps/tests/test_forms.py
+++ peacecorps/peacecorps/tests/test_forms.py
@@ -1,20 +1,20 @@
 from django.test import TestCase
-from peacecorps.forms import DonationPaymentForm
-
+from peacecorps.forms import IndividualDonationForm
 
 class DonationPaymentTests(TestCase):
-    def test_us_donation_required(self):
+    def test_individual_donation_required(self):
         """ Check the minimum required data.  """
         form_data = {
             'donor_type': 'Individual',
             'name': 'William Williams',
             'street_address': '1 Main Street',
             'city': 'Anytown',
+            'country': 'USA',
             'state': 'MD',
             'zip_code': '20852',
             'payment_type': 'credit-card',
             'information_consent': 'vol-consent-yes'
         }
-        form = DonationPaymentForm(data=form_data)
+        form = IndividualDonationForm(data=form_data)
         form_validity = form.is_valid()
         self.assertTrue(form.is_valid())
