e381649681c5c903391049162f72117375f99f74
CM Lubinski <christopher.lubinski@gsa.gov>
2014-10-06 14:19:20 +0000

    Add dedication contact field; JS to change honoree/family language

--- peacecorps/peacecorps/forms.py
+++ peacecorps/peacecorps/forms.py
@@ -31,6 +31,8 @@ class DedicationForm(forms.Form):
     dedication_type = forms.ChoiceField(
         widget=forms.RadioSelect, choices=DEDICATION_TYPE_CHOICES,
         initial='in-honor', required=False)
+    dedication_contact = forms.CharField(
+        label="Family Contact", max_length=40, required=False)
     dedication_email = forms.EmailField(label="Email", required=False)
     dedication_address = forms.CharField(
         label="Mailing Address", max_length=255, required=False)
--- peacecorps/peacecorps/static/peacecorps/js/donation.js
+++ peacecorps/peacecorps/static/peacecorps/js/donation.js
@@ -20,10 +20,32 @@ var Init = {
     dedication.change(function() {
       dedicationDiv.toggle(dedication.is(':checked'));
     });
+  },
+
+  //  In-memory donations have a separate field + wording
+  inMemoryChanges: function() {
+    var selectors = $('input[name=dedication_type'),
+        honoreeSwap = $('.honoree_swap');
+
+    $('input[name=dedication_type]').change(function() {
+      //  "this" refers to the *selected* radio button
+      var inMemory = $(this).val() === 'in-memory';
+
+      $('#dedication_contact_wrapper').toggle(inMemory);
+      honoreeSwap.each(function(idx, el) {
+        var $el = $(el);
+        if (inMemory) {
+          $el.html($el.html().replace('honoree', 'family'));
+        } else {
+          $el.html($el.html().replace('family', 'honoree'));
+        }
+      });
+    });
   }
 };
 
 $(document).ready(function() {
   Init.countryRequirements();
   Init.dedicationFields();
+  Init.inMemoryChanges();
 });
