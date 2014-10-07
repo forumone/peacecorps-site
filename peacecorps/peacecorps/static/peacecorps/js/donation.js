a6f39e86285cfa512eb9f1bb9e1de4e15b31580a
CM Lubinski <christopher.lubinski@gsa.gov>
2014-10-07 21:41:18 +0000

    Combine forms. Add requirement validation checking

--- peacecorps/peacecorps/static/peacecorps/js/donation.js
+++ peacecorps/peacecorps/static/peacecorps/js/donation.js
@@ -1,7 +1,21 @@
 'use strict';
 
 //  Namespace
+//  Note that we set up an event listener and call it immediately to check the
+//  initial state
 var Init = {
+  //  Individual and Organization have different fields
+  donorTypeFields: function() {
+    var orgFields = $('.shown-with-organization'),
+        indFields = $('.shown-with-individual');
+    $('input[name=donor_type]').change(function() {
+      var isOrg = $('input[name=donor_type]:checked').val() === 'Organization';
+
+      orgFields.toggle(isOrg);
+      indFields.toggle(!isOrg);
+    }).change();
+  },
+
   //  State/Zip are only marked "required" if country == USA
   countryRequirements: function() {
     var country = $('#id_country'),
@@ -9,7 +23,7 @@ var Init = {
 
     country.change(function() {
       countryReqLabels.toggleClass('required', country.val() === 'USA');
-    });
+    }).change();
   },
 
   //  Donations in dedication to someone have additional fields
@@ -19,17 +33,17 @@ var Init = {
 
     dedication.change(function() {
       dedicationDiv.toggle(dedication.is(':checked'));
-    });
+    }).change();
   },
 
   //  In-memory donations have a separate field + wording
   inMemoryChanges: function() {
-    var selectors = $('input[name=dedication_type'),
-        honoreeSwap = $('.honoree_swap');
+    var honoreeSwap = $('.honoree_swap');
 
     $('input[name=dedication_type]').change(function() {
       //  "this" refers to the *selected* radio button
-      var inMemory = $(this).val() === 'in-memory';
+      var isMemory = ($('input[name=dedication_type]:checked').val()
+                      === 'in-memory');
 
       $('#dedication_contact_wrapper').toggle(inMemory);
       honoreeSwap.each(function(idx, el) {
@@ -40,11 +54,12 @@ var Init = {
           $el.html($el.html().replace('family', 'honoree'));
         }
       });
-    });
+    }).change();
   }
 };
 
 $(document).ready(function() {
+  Init.donorTypeFields();
   Init.countryRequirements();
   Init.dedicationFields();
   Init.inMemoryChanges();
