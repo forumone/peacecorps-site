6a80cccf6a4dbee3ee91a3475b5d17684cd36d24
Annalee Flower Horne <leeflowerh@gmail.com>
2014-10-20 10:47:02 -0400

    migrations.

--- peacecorps/peacecorps/migrations/0005_auto_20141016_1641.py
+++ peacecorps/peacecorps/migrations/0005_auto_20141016_1641.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='countryfund',
             name='description',
-            field=models.TextField(default='<p>Proin at sem vel lorem tincidunt lobortis viverra a urna. Praesent at congue mi, sed dignissim arcu. Quisque quis facilisis ligula. Pellentesque dictum est nulla. In sagittis arcu in purus vulputate imperdiet. Proin nec risus lacinia, dignissim felis nec, porta sem. Etiam tortor magna, porta eget dui ut, consequat aliquet dui. Morbi varius laoreet risus, ac auctor magna semper ac. Vestibulum libero dui, tristique non ex sed, condimentum pharetra ex. Nam quis lorem tempor, luctus sapien nec, congue lorem. Nulla elementum vel tortor euismod convallis. Vestibulum aliquet pulvinar lacus id sagittis. Aenean ornare diam ac odio eleifend, vitae fermentum nisl malesuada.</p><p>Duis vitae ullamcorper sem, at vehicula orci. Donec dapibus, enim a posuere tincidunt, turpis arcu tristique odio, sed vestibulum mi ante nec leo. Curabitur hendrerit lacinia ultrices. Aliquam eget lacinia quam. Proin mattis massa massa, vitae vestibulum quam facilisis a. Morbi dolor quam, tempor sit amet scelerisque nec, auctor id eros.'),
+            field=models.TextField(),
             preserve_default=False,
         ),
         migrations.AlterField(
