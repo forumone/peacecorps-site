06a0ecebe68156c4f76958e89583ba3ab0fe31c6
Sean Herron <sean.herron@gsa.gov>
2014-10-23 16:19:53 -0400

    spelling fix in error message

--- peacecorps/peacecorps/admin.py
+++ peacecorps/peacecorps/admin.py
@@ -27,7 +27,7 @@ def password_validator(password):
     if not any(x.isdigit() for x in password):
         errors.append('Password needs at least one number.')
     if not any((x in valid_special_chars) for x in password):
-        errors.append('Password required a special character.')
+        errors.append('Password needs a special character.')
     if not len(password) >= 20:
         errors.append('Password must be at least 20 characters in length.')
 
