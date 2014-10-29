788971d8cda199a26d22b88ea4cc3207bb5fb4e5
CM Lubinski <christopher.lubinski@gsa.gov>
2014-10-29 20:10:10 +0000

    Must update the test settings as well

--- peacecorps/peacecorps/settings/test.py
+++ peacecorps/peacecorps/settings/test.py
@@ -11,6 +11,21 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = '/static/'
+INSTALLED_APPS += (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'tinymce',
+    'paygov'
+)
 
-INSTALLED_APPS += ('paygov',)
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
