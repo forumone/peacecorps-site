d61fd948c7ec5147b81b7603fea4c18a3aa7f9ee
CM Lubinski <christopher.lubinski@gsa.gov>
2014-12-16 18:02:48 +0000

    Image resizing sends to default media handler

--- peacecorps/contenteditor/imagesizing.py
+++ peacecorps/contenteditor/imagesizing.py
@@ -1,29 +1,29 @@
+import os
+import tempfile
+
 from django.conf import settings
+from django.core.files.storage import default_storage
 from PIL import Image
 
 
-def resize(file_):
+# We'll be mutating an image, so scale from larger to smaller
+SIZES = (('lg', 1200, 1200), ('md', 900, 900), ('sm', 500, 500),
+         ('thm', 300, 300))
 
-    def resize(image, size, ext, filename):
-        image.thumbnail(size, Image.ANTIALIAS)
 
-        path = settings.MEDIA_ROOT + settings.SIRTREVOR_UPLOAD_PATH
+def resize(original_file):
+    """When an image has been uploaded, resize it to thumbnails, etc."""
 
-        image.save(path+filename + '-' + ext + '.' + image.format.lower(),
-                   image.format)
-        return None
+    filename = original_file.name.split('.')[0]
 
-    filename = file_.name.split('.')[0]
+    image = Image.open(original_file)
+    for ext, width, height in SIZES:
+        with tempfile.TemporaryFile() as buffer_file:
+            image.thumbnail((width, height), Image.ANTIALIAS)
+            path = os.path.join(
+                settings.SIRTREVOR_UPLOAD_PATH,
+                filename + '-' + ext + '.' + image.format.lower())
+            image.save(buffer_file, image.format.lower())
+            default_storage.save(path, buffer_file)
 
-    lg = (1200, 1200)
-    md = (900, 900)
-    sm = (500, 500)
-    thm = (300, 300)
-
-    im = Image.open(file_)
-    resize(im, lg, 'lg', filename)
-    resize(im, md, 'md', filename)
-    resize(im, sm, 'sm', filename)
-    resize(im, thm, 'thm', filename)
-
-    return file_
+    return original_file
