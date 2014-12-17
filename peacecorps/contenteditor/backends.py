dc1e45e94da423d22a041475a8aef2ae3ed477b7
CM Lubinski <christopher.lubinski@gsa.gov>
2014-12-17 21:31:50 +0000

    Account for boto bug

--- peacecorps/contenteditor/backends.py
+++ peacecorps/contenteditor/backends.py
@@ -1,4 +1,5 @@
 import logging
+from urllib import parse as urlparse
 
 from django.conf import settings
 from django.contrib.auth import backends
@@ -47,23 +48,36 @@ class LoggingStorage(LazyObject):
         return result
 
 
-class MediaS3Storage(S3BotoStorage):
-    """Pass through to S3BotoStorage, save that the bucket name is
-    predefined"""
+class ConfiguredStorage(S3BotoStorage):
+    """Base class for Media and Static Storage. This makes it easy to have
+    separate bucket names for media and static assets. It also hacks around
+    this bug: https://github.com/boto/boto/pull/2637"""
     def __init__(self, *args, **kwargs):
         if not args and 'bucket' not in kwargs:
-            super(MediaS3Storage, self).__init__(
-                getattr(settings, 'AWS_MEDIA_BUCKET_NAME', ''), **kwargs)
+            super(ConfiguredStorage, self).__init__(
+                self.default_bucket_name(), **kwargs)
         else:
-            super(MediaS3Storage, self).__init__(**kwargs)
+            super(ConfiguredStorage, self).__init__(**kwargs)
+
+    def url(self, name):
+        result = super(ConfiguredStorage, self).url(name)
+        if self.querystring_auth:
+            return result
+        else:
+            parsed = urlparse.urlparse(result)
+            new_qsl = filter(lambda pair: pair[0] != 'x-amz-security-token',
+                             urlparse.parse_qsl(parsed.query))
+            new_qs = urlparse.urlencode(list(new_qsl), doseq=True)
+            return urlparse.urlunparse([
+                parsed.scheme, parsed.netloc, parsed.path, parsed.params,
+                new_qs, parsed.fragment])
 
 
-class StaticS3Storage(S3BotoStorage):
-    """Pass through to S3BotoStorage, save that the bucket name is
-    predefined"""
-    def __init__(self, *args, **kwargs):
-        if not args and 'bucket' not in kwargs:
-            super(StaticS3Storage, self).__init__(
-                getattr(settings, 'AWS_STATIC_BUCKET_NAME', ''), **kwargs)
-        else:
-            super(StaticS3Storage, self).__init__(**kwargs)
+class MediaS3Storage(ConfiguredStorage):
+    def default_bucket_name(self):
+        return getattr(settings, 'AWS_MEDIA_BUCKET_NAME', '')
+
+
+class StaticS3Storage(ConfiguredStorage):
+    def default_bucket_name(self):
+        return getattr(settings, 'AWS_STATIC_BUCKET_NAME', '')
