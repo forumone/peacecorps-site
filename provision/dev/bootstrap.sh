6e283f2557c7ca707745013b9c614587d3bde082
Sean Herron <sean.herron@gsa.gov>
2015-03-26 15:49:27 -0400

    added vagrantfile and provisioning for dev environment

---
+++ provision/dev/bootstrap.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+sudo apt-get update -y
+sudo apt-get install python-pip -y
+sudo apt-get install git -y
+
+sudo apt-get install libevent-dev -y
+sudo apt-get install libpq-dev -y
+sudo apt-get install python-virtualenv -y
+sudo apt-get install python-dev -y
+sudo apt-get install libbz2-dev -y
+sudo apt-get install libsqlite3-dev -y
+sudo apt-get install libreadline-dev -y
+
+sudo su vagrant <<'EOF'
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+. ~/.bash_profile
+pyenv install 3.4.1
+pyenv rehash
+pyenv virtualenv 3.4.1 peacecorps
+pyenv rehash
+pyenv activate peacecorps
+pip install -r /vagrant/requirements.txt
+pip install -r /vagrant/requirements-dev.txt
+cd /vagrant/peacecorps
+python manage.py migrate
+python manage.py loaddata countries issues global-general
+echo 'cd /vagrant/peacecorps' >> ~/.bash_profile
+echo 'pyenv activate peacecorps' >> ~/.bash_profile
+EOF
